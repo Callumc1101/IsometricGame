88d21f4026520d067c513cae55d12cb133b4a80f
Callumc1101 <callumc1101@gmail.com>
2025-08-17 22:18:12 +0100

    w

--- IsometricGame/Assets/Scripts/PlayerMovement.cs
+++ IsometricGame/Assets/Scripts/PlayerMovement.cs
@@ -3,9 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class NewBehaviourScript : MonoBehaviour{
+public class NewBehaviourScript : MonoBehaviour
+{
 
     private Vector2 movement;
-    private void OnMovement(InputValue value) {
-        movement = value.Get<Vector2>();
+    private Rigidbody2D rb;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
     }
+        
+
+
+            private void OnMovement(InputValue value) => movement = value.Get<Vector2>();
+
+            private void FixedUpadte() => rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
+        }
+
+
